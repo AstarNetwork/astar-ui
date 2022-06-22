e46c6b5bdb06cc12d71b6d213599ca2427775155
impelcrypto <impelcrypto@gmail.com>
2022-06-22 15:06:27 +0800

    feat: added icons

---
+++ packages/Icons/src/GoogleMaterialIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <head>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+    />
+  </head>
+</template>
---
+++ packages/Icons/src/IconHide.vue
@@ -0,0 +1,26 @@
+<template>
+  <GoogleMaterialIcon />
+  <span class="material-symbols-outlined icon" :style="size && `font-size: ${size}px`"> visibility_off </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import GoogleMaterialIcon from './GoogleMaterialIcon.vue'
+
+export default defineComponent({
+  components:{ GoogleMaterialIcon },
+  props:{
+    size: {
+      type: String,
+      required: false,
+      default:""
+    },
+  }
+});
+</script>
+
+<style scoped>
+.icon {
+  cursor: default;
+}
+</style>
