78c2b2236f0ce43ebc109e66c1feb9ac4d68c79d
impelcrypto <impelcrypto@gmail.com>
2023-03-08 15:57:18 +0800

    feat: added equip and unequip icons

---
+++ src/stories/Icons/IconUnequip.stories.js
@@ -0,0 +1,11 @@
+import IconUnequip from "packages/Icons/src/IconUnequip.vue";
+
+export default {
+  title: "Icons/IconUnequip",
+  component: IconUnequip,
+};
+
+export const DefaultIcon = () => ({
+  components: { IconUnequip },
+  template: "<IconUnequip />",
+});
