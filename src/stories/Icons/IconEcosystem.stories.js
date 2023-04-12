a2886db3f59ed45ef1eaab744bbeac0c72120152
Will <korea651@hanmail.net>
2023-04-12 10:36:03 +0900

    added icons (#58)

---
+++ src/stories/Icons/IconEcosystem.stories.js
@@ -0,0 +1,11 @@
+import IconEcosystem from "packages/Icons/src/IconEcosystem.vue";
+
+export default {
+  title: 'Icons/IconEcosystem',
+  component: IconEcosystem
+};
+
+export const DefaultIcon = () => ({
+  components: { IconEcosystem },
+  template: '<IconEcosystem />',
+});
