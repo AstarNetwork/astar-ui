1db4319adbcc86639fd213281f679f6c71e7c7b2
impelcrypto <impelcrypto@gmail.com>
2024-01-18 18:33:59 +0800

    config: added .prettierc

--- packages/Modals/index.ts
+++ packages/Modals/index.ts
@@ -1,23 +1,17 @@
-import { App, Plugin } from "vue";
-import SimpleModal from "./src/SimpleModal.vue";
-import ModalDrawer from "./src/ModalDrawer.vue";
-import DefaultModal from "./src/DefaultModal.vue";
-import { fadeDuration } from "./src/index";
-import IconBackWithColor from "./src/IconBackWithColor.vue";
+import { App, Plugin } from 'vue';
+import SimpleModal from './src/SimpleModal.vue';
+import ModalDrawer from './src/ModalDrawer.vue';
+import DefaultModal from './src/DefaultModal.vue';
+import { fadeDuration } from './src/index';
+import IconBackWithColor from './src/IconBackWithColor.vue';
 
 export const ModalsPlugin: Plugin = {
   install(app: App) {
-    app.component("astar-simple-modal", SimpleModal);
-    app.component("astar-modal-drawer", ModalDrawer);
-    app.component("astar-default-modal", DefaultModal);
-    app.component("astar-icon-back-with-color", IconBackWithColor);
+    app.component('astar-simple-modal', SimpleModal);
+    app.component('astar-modal-drawer', ModalDrawer);
+    app.component('astar-default-modal', DefaultModal);
+    app.component('astar-icon-back-with-color', IconBackWithColor);
   },
 };
 
-export {
-  SimpleModal,
-  ModalDrawer,
-  DefaultModal,
-  fadeDuration,
-  IconBackWithColor,
-};
+export { SimpleModal, ModalDrawer, DefaultModal, fadeDuration, IconBackWithColor };
